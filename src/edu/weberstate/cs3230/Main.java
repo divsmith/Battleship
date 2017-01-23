1a7049c7132d1009d6257928dd116a8df1fd1b0a
Parker Smith <parker@parkersmith.us>
2017-01-23 13:06:57 -0700

    Output grid

--- src/edu/weberstate/cs3230/Main.java
+++ src/edu/weberstate/cs3230/Main.java
@@ -1,11 +1,51 @@
 package edu.weberstate.cs3230;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        String input;
         Tile[][] grid = new Tile[10][10];
 
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                grid[i][j] = new Tile();
+            }
+        }
+
+        printGrid(grid);
+    }
+
+    public static void printGrid(Tile[][] grid)
+    {
+        char[] rows = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
+        System.out.print("  1 2 3 4 5 6 7 8 9 10\n");
+        int count = 0;
+
+        for (char row : rows)
+        {
+            System.out.print(row + " ");
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (grid[count][i].marked)
+                {
+                    System.out.print("X ");
+                }
+                else
+                {
+                    System.out.print("_ ");
+                }
+            }
+            System.out.println();
+            count++;
+        }
 
     }
 }
