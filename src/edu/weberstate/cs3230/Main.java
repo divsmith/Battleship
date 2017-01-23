3d2734b523907f533e8e28f2afb43c8e0aba9a92
Parker Smith <parker@parkersmith.us>
2017-01-23 13:30:13 -0700

    Finish input checking and output

--- src/edu/weberstate/cs3230/Main.java
+++ src/edu/weberstate/cs3230/Main.java
@@ -8,8 +8,9 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        String input;
+        String input = "";
         Tile[][] grid = new Tile[10][10];
+        char[] rows = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
 
         for (int i = 0; i < 10; i++)
         {
@@ -19,13 +20,43 @@ public class Main {
             }
         }
 
-        printGrid(grid);
+        do {
+            printGrid(grid);
+            System.out.println("Enter a position as \"rowcolumn\" or enter \'q\' to quit");
+            input = scanner.nextLine();
+
+            if (input.length() != 2 && !input.equals("q"))
+            {
+                System.out.println("Please enter a row immediately followed by a column");
+            }
+            else if (!input.equals("q"))
+            {
+                if (!(new String(rows).contains(String.valueOf(input.charAt(0)))))
+                {
+                    System.out.println("Please enter a valid row");
+                }
+                else if (Character.getNumericValue(input.charAt(1)) > 9 || Character.getNumericValue(input.charAt(1)) < 0)
+                {
+                    System.out.println("Please enter a valid column");
+                }
+                else if (grid[new String(rows).indexOf((input.charAt(0)))][Character.getNumericValue(input.charAt(1))].marked)
+                {
+                    System.out.println("That tile is already marked");
+                }
+                else
+                {
+                    grid[new String(rows).indexOf((input.charAt(0)))][Character.getNumericValue(input.charAt(1))].marked = true;
+                }
+            }
+
+        } while (!input.equals("q"));
+
     }
 
     public static void printGrid(Tile[][] grid)
     {
         char[] rows = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
-        System.out.print("  1 2 3 4 5 6 7 8 9 10\n");
+        System.out.print("  0 1 2 3 4 5 6 7 8 9\n");
         int count = 0;
 
         for (char row : rows)
