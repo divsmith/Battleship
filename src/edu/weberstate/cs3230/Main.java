b282da4a47d8941a21a82d3ce329e80000a87103
Parker Smith <parker@parkersmith.us>
2017-01-23 12:45:03 -0700

    Create tile array

--- src/edu/weberstate/cs3230/Main.java
+++ src/edu/weberstate/cs3230/Main.java
@@ -3,6 +3,9 @@ package edu.weberstate.cs3230;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Tile[][] grid = new Tile[10][10];
+
+
     }
 }
