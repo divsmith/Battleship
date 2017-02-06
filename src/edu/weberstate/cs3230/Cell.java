8fecdee202b7fe9cadb27cbd42aac463377d7e29
Parker Smith <parker@parkersmith.us>
2017-02-06 16:40:53 -0700

    Add contents to cell class

--- src/edu/weberstate/cs3230/Cell.java
+++ src/edu/weberstate/cs3230/Cell.java
@@ -4,5 +4,29 @@ package edu.weberstate.cs3230;
  * Created by parker on 2/6/17.
  */
 public class Cell {
+    private boolean isMarked;
+    private boolean isHit;
+    private Ship ship;
 
+    public Cell()
+    {
+        this.isMarked = false;
+        this.isHit = false;
+        this.ship = null;
+    }
+
+    public boolean hasShip()
+    {
+        return !(this.ship == null);
+    }
+
+    public void setShip(Ship ship)
+    {
+        this.ship = ship;
+    }
+
+    public Ship getShip()
+    {
+        return this.ship;
+    }
 }
