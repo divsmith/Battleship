d5015eeeba22da968cd2eec30dc2efd6e26980c6
Parker Smith <parker@parkersmith.us>
2017-02-06 16:48:09 -0700

    Add hitstate, name, getter, setting to ship class

--- src/edu/weberstate/cs3230/Ship.java
+++ src/edu/weberstate/cs3230/Ship.java
@@ -4,5 +4,22 @@ package edu.weberstate.cs3230;
  * Created by parker on 2/6/17.
  */
 abstract public class Ship {
+    private String name;
 
+    public enum Hitstate
+    {
+        Hit,
+        Sunk,
+        Invalid
+    }
+
+    protected Ship(String name)
+    {
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return this.name;
+    }
 }
