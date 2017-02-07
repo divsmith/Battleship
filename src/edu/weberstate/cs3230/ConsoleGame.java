dc96ee5a7b72e712ddc8839299d78c6a97c77437
Parker Smith <parker@parkersmith.us>
2017-02-06 17:42:25 -0700

    Rename Gameboard to ConsoleGame

--- src/edu/weberstate/cs3230/ConsoleGame.java
+++ src/edu/weberstate/cs3230/ConsoleGame.java
@@ -3,11 +3,11 @@ package edu.weberstate.cs3230;
 /**
  * Created by parker on 2/6/17.
  */
-public class Gameboard {
+public class ConsoleGame {
 
     private Cell[][] grid;
 
-    public Gameboard(int size)
+    public ConsoleGame(int size)
     {
         grid = new Cell[size][size];
     }
