98274ac493f4c9839407b3f9ee2c59c2a26a2514
Parker Smith <parker@parkersmith.us>
2017-03-04 20:34:54 -0700

    Add additional functionality and testing to player

--- src/edu/weberstate/cs3230/Player.java
+++ src/edu/weberstate/cs3230/Player.java
@@ -8,12 +8,11 @@ import java.util.List;
  */
 public class Player {
     private static String DEFAULT_NAME = "default";
-    private static int nextPlayerNumber = 1;
 
     private Grid grid;
-    private List<Ship> ships = new ArrayList<Ship>();
+    protected List<Ship> ships = new ArrayList<Ship>();
+    protected List<Ship> unplacedShips = new ArrayList<Ship>();
     private String name;
-    private int playerNumber;
 
     public Player()
     {
@@ -25,8 +24,22 @@ public class Player {
         setName(name);
 
         grid = new Grid();
-        this.playerNumber = Player.nextPlayerNumber;
-        Player.nextPlayerNumber++;
+
+        unplacedShips.add(new Battleship());
+        unplacedShips.add(new Carrier());
+        unplacedShips.add(new Destroyer());
+        unplacedShips.add(new Patrol());
+        unplacedShips.add(new Submarine());
+    }
+
+    public boolean hasShipsToPlace()
+    {
+        return unplacedShips.size() > 0;
+    }
+
+    public List<Ship> getUnplacedShips()
+    {
+        return unplacedShips;
     }
 
     public void setName(String name)
@@ -39,13 +52,19 @@ public class Player {
         return this.name;
     }
 
-    public boolean placeShip(Ship ship, Placement placement)
+    public boolean placeShip(int index, Placement placement)
     {
-        boolean placed = this.grid.placeShip(ship, placement);
+        if (index > unplacedShips.size() - 1)
+        {
+            return false;
+        }
+
+        boolean placed = this.grid.placeShip(unplacedShips.get(index), placement);
 
         if (placed)
         {
-            ships.add(ship);
+            ships.add(unplacedShips.get(index));
+            unplacedShips.remove(index);
         }
 
         return placed;
@@ -54,10 +73,5 @@ public class Player {
     public List<Ship> getShips()
     {
         return this.ships;
-    }
-
-    public int getPlayerNumber()
-    {
-        return playerNumber;
     }
 }
--- test/edu/weberstate/cs3230/PlayerTest.java
+++ test/edu/weberstate/cs3230/PlayerTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.*;
 /**
  * Created by parker on 3/4/17.
  */
-class PlayerTest {
+class PlayerTest extends Player{
     @Test
     void name_is_set_and_returned()
     {
@@ -20,38 +20,54 @@ class PlayerTest {
     }
 
     @Test
-    void player_number_is_set_correctly()
-    {
-        Player player1 = new Player();
-        Player player2 = new Player();
-
-        Assertions.assertEquals(1, player1.getPlayerNumber());
-        Assertions.assertEquals(2, player2.getPlayerNumber());
-    }
-
-    @Test
     void validly_placed_ships_are_added_to_list()
     {
         Player player = new Player();
-        Patrol patrol = new Patrol();
+        Ship ship = player.getUnplacedShips().get(0);
+        player.placeShip(0, new Placement('a', 0, 'v'));
 
-        player.placeShip(patrol, new Placement('a', 0, 'h'));
-
-        List<Ship> list = player.getShips();
-
-        Assertions.assertEquals(patrol, list.get(0));
+        Assertions.assertFalse(player.getUnplacedShips().contains(ship));
+        Assertions.assertTrue(player.getShips().contains(ship));
     }
 
     @Test
     void invalidly_placed_ships_are_not_added_to_list()
     {
         Player player = new Player();
-        Patrol patrol = new Patrol();
+        Ship ship = player.getUnplacedShips().get(0);
+        player.placeShip(0, new Placement('w', 0, 'h'));
 
-        player.placeShip(patrol, new Placement('w', 0, 'h'));
+        Assertions.assertTrue(player.getUnplacedShips().contains(ship));
+        Assertions.assertFalse(player.getShips().contains(ship));
+    }
 
-        List<Ship> list = player.getShips();
+    @Test
+    void default_ship_list_is_not_empty()
+    {
+        Player player = new Player();
 
-        Assertions.assertEquals(0, list.size());
+        Assertions.assertNotEquals(0, player.getUnplacedShips().size());
+    }
+
+    @Test
+    void hasShipsToPlace_returns_correctly()
+    {
+        Player player = new Player();
+
+        Assertions.assertTrue(player.hasShipsToPlace());
+        player.placeShip(0, new Placement('a', 0, 'h'));
+        player.placeShip(0, new Placement('b', 0, 'h'));
+        player.placeShip(0, new Placement('c', 0, 'h'));
+        player.placeShip(0, new Placement('d', 0, 'h'));
+        player.placeShip(0, new Placement('e', 0, 'h'));
+        Assertions.assertFalse(player.hasShipsToPlace());
+    }
+
+    @Test
+    void cannot_place_invalid_ship()
+    {
+        Player player = new Player();
+
+        Assertions.assertFalse(player.placeShip(5, new Placement('a', 0, 'h')));
     }
 }
