c32f572fea42483e6f813d89c7e116ef13f5989c
Parker Smith <parker@parkersmith.us>
2017-03-12 20:44:24 -0600

    Add default uncaught exception handler

--- src/edu/weberstate/cs3230/Main.java
+++ src/edu/weberstate/cs3230/Main.java
@@ -6,6 +6,11 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
+        Thread.currentThread().setUncaughtExceptionHandler((t, e) -> {
+            System.out.println("Sorry, something unexpected happened. The stacktrace has been logged.");
+        });
+
         IGame game = new ConsoleGame();
 
         game.play();
